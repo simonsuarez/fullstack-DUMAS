3d6059fc8fe036486d7d0e3e141df1783e977dc4
Simon Suarez <simon.suarez.ovalle@gmail.com>
2025-10-28 17:10:07 -0300

    cambio BD y cambio en las versiones de las dependencias que requiere el backend

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # PostgreSQL - usa estas credenciales
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin12345@localhost:5432/gatos_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin12345@localhost:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
