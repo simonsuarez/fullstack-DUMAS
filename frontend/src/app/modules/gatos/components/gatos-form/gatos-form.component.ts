841cc1e60851b4edf9a9f0a27c8fb7b8862f8215
Simon Suarez <simon.suarez.ovalle@gmail.com>
2025-10-27 22:01:36 -0300

    Se crea el formulario para ingresar datos del gato

--- frontend/src/app/modules/gatos/components/gatos-form/gatos-form.component.ts
+++ frontend/src/app/modules/gatos/components/gatos-form/gatos-form.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+// frontend/src/app/modules/gatos/components/gatos-form/gatos-form.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GatosService } from '../../../../services/gatos/gatos.service';
+import { Gato, GatoCreate } from '../../../../models/gatos/gato.model';
 
 @Component({
   selector: 'app-gatos-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './gatos-form.component.html',
   styleUrl: './gatos-form.component.css'
 })
-export class GatosFormComponent {
+export class GatosFormComponent implements OnInit {
+  gato: GatoCreate = {
+    nombre: '',
+    raza: '',
+    edad: 0,
+    foto: ''
+  };
+  
+  isEdit = false;
+  gatoId?: number;
 
+  constructor(
+    private gatosService: GatosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.gatoId = Number(this.route.snapshot.paramMap.get('id'));
+    this.isEdit = !!this.gatoId;
+    
+    if (this.isEdit) {
+      this.cargarGato();
+    }
+  }
+
+  cargarGato(): void {
+    if (this.gatoId) {
+      this.gatosService.getGato(this.gatoId).subscribe({
+        next: (gato) => {
+          this.gato = gato;
+        },
+        error: (error) => {
+          console.error('Error cargando gato:', error);
+        }
+      });
+    }
+  }
+
+  guardarGato(): void {
+    if (this.isEdit && this.gatoId) {
+      this.gatosService.updateGato(this.gatoId, this.gato).subscribe({
+        next: () => {
+          this.router.navigate(['/gatos']);
+        },
+        error: (error) => {
+          console.error('Error actualizando gato:', error);
+        }
+      });
+    } else {
+      this.gatosService.createGato(this.gato).subscribe({
+        next: () => {
+          this.router.navigate(['/gatos']);
+        },
+        error: (error) => {
+          console.error('Error creando gato:', error);
+        }
+      });
+    }
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/gatos']);
+  }
 }
--- frontend/src/app/modules/gatos/gatos-routing.module.ts
+++ frontend/src/app/modules/gatos/gatos-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GatosListComponent } from './components/gatos-list/gatos-list.component';
+import { GatosFormComponent } from './components/gatos-form/gatos-form.component';
 
 const routes: Routes = [
-  { path: '', component: GatosListComponent }
+  { path: '', component: GatosListComponent },
+  { path: 'crear', component: GatosFormComponent },
+  { path: 'editar/:id', component: GatosFormComponent }
 ];
 
 @NgModule({
